970abb9ab73ae3969a7fae0eccb87081f047099f
Sulamaxx <sjeewaranga@gmail.com>
2023-05-10 14:01:13 +0530

    Create customerProductView.php

---
+++ customerProductView.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<?php 
+
+?>
+
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="header.css" />
+</head>
+
+<body style="margin: 0px;">
+    <div class="container-fluid text-center">
+        <div class="row" id="customerProductView">
+
+
+
+        </div>
+    </div>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="custom.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
