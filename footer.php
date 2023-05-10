5d6fb32b4a306c869ba77b377494111e7b8383a4
Sulamaxx <sjeewaranga@gmail.com>
2023-05-10 14:00:59 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+
+    <footer class="text-white pt-3 pb-4" style="background-color: #377ca3;">
+
+        <div class="col-12 text-md-start">
+            <div class="row text-sm-center text-md-start">
+
+                <div class="col-12 col-md-3 col-lg-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 text-warning fs-3 fw-bold">Online Shop</h5>
+                    <p style="text-align: justify;" class="text-white">Here we are the Online Shop.lk&trade; to support you for accomplish your dessire by buying high quality products.</p>
+                </div>
+
+                <div class="col-12 col-md-3 col-lg-2 mx-auto mt-5 pt-4">
+                    <p class="text-white" style="text-align: justify;">Copyright &copy;2021 Online Shop All Right Reserved</p>
+                </div>
+
+                <div class="col-12 col-md-2 col-lg-2 mx-auto mt-5 pt-4">
+                    <div class="text-center text-md-start">
+
+                        <ul class="list-unstyled list-inline">
+
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-facebook" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+                            &nbsp;
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-twitter" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+                            &nbsp;
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-google" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+                            &nbsp;
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-linkedin" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+                            &nbsp;
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-youtube" style="font-size: 22px;"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 col-lg-3 mx-auto text-start mt-3">
+
+                    <h5 class="text-uppercase text-warning mb-4 fw-bold">Contact</h5>
+                    <p class="text-white"><i class="bi bi-house-door-fill text-warning"></i>&nbsp;&nbsp;Maradana,Colombo 10,Sri Lanka.</p>
+                    <p class="text-white"><i class="bi bi-envelope-fill text-warning"></i>&nbsp;&nbsp;onlineshop@gmail.com</p>
+                    <p class="text-white"><i class="bi bi-telephone-fill text-warning"></i>&nbsp;&nbsp;+94 112 369852</p>
+                    <p class="text-white"><i class="bi bi-printer-fill text-warning"></i>&nbsp;&nbsp;+94 112 147852</p>
+
+                </div>
+
+                <div class="col-12">
+                    <hr class="hr-break-1" />
+                </div>
+
+
+            </div>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
