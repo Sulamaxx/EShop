29678fc057755382699190162f025f5a53fd8519
Sulamaxx <sjeewaranga@gmail.com>
2023-05-10 13:59:34 +0530

    Create addproduct.php

---
+++ addproduct.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <!-- <title>eShop | Add Product</title> -->
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="icon" href="resources/logo.png" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+
+    <div class="container-fluid bg-warning px-4">
+
+        <div class="row gy-3 " id="addproductbox">
+
+         
+
+        </div>
+    </div>
+
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
