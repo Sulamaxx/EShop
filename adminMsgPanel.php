db1fc0191618d47a9ea3d4d5ce45796a90f939e5
Sulamaxx <sjeewaranga@gmail.com>
2023-05-10 14:00:18 +0530

    Create adminMsgPanel.php

---
+++ adminMsgPanel.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message Panel</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+
+
+    <div class="container-fluid" style="margin-top: 50px;">
+        <div class="row justify-content-center" id="msgview">
+
+           
+
+
+        </div>
+    </div>
+
+
+
+
+    <script src="script.js"></script>
+    <script src="custom.js"></script>
+
+</body>
+
+</html>
