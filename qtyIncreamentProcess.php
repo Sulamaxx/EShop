92f36e81a138cc3475dfcc92175f7853b6b60d73
Sulamaxx <sjeewaranga@gmail.com>
2023-05-10 14:01:58 +0530

    Create qtyIncreamentProcess.php

---
+++ qtyIncreamentProcess.php
@@ -0,0 +1,15 @@
+<?php
+require "connection.php";
+$pid=$_POST["pid"];
+$qty=$_POST["qty"];
+
+
+$result=Database::search("SELECT * FROM `product` WHERE `id`='".$pid."'");
+$data=$result->fetch_assoc();
+
+$newPrice=$qty*$data["price"];
+
+// echo $pid;
+// echo $qty;
+?>
+Rs. <?php echo $newPrice; ?>.00
